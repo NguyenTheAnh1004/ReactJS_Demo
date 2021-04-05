70b5e44f340204e5a4672f5c2cbcbc95dfacdac9
Nguyễn Hữu Khương <Khuong@DESKTOP-TFJ30ML>
2021-04-05 21:58:21 +0700

    devA

---
+++ src/Component/devA.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class devA extends Component {
+    render() {
+        return (
+            <div>
+                ádasdasds
+            </div>
+        )
+    }
+}
